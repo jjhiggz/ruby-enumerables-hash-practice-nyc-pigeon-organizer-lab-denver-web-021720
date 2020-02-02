616028e319d2922871427ca1c7af25c672b48787
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-02 21:45:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -41,7 +41,7 @@ a = color.reduce({}) do |memo,(key, value)|
 end
 
 b=gender.reduce({})do |memo,(key, value)|
-binding.pry
+#binding.pry
 i=0
  while i<gender[key].length
     if newHash[value[i].to_sym][:gender]
