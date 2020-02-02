63731ec8cf075e68772f409cc6bf2ca117a33f16
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-02 21:23:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,35 +24,19 @@ data = {
   # write your code here!
 newHash = {}
 color = data[:color]
-<<<<<<< HEAD
 a = color.reduce({}) do |memo,(key, value)|
   i = 0
-#binding.pry
+
   while i<color[key].length
-      #binding.pry
     if newHash[value[i].to_sym]
-      binding.pry
+    
       newHash[value[i].to_sym].push(key.to_s)
     else
       newHash[value[i].to_sym] = [key.to_s]
-=======
-a = color.reduce({}) do |memo, key, value|
-  i = 0
-binding.pry
-  while i<color[key].length
-    if newHash[value.to_sym]
-      newHash[value.to_sym] << key.to_s
-    else
-      newHash[value.to_sym] = key.to_s
->>>>>>> ea9487d23c7c63b73665c7eb47bbbcdd798bc7fa
     end
     i=i+1
   end
 end
-<<<<<<< HEAD
   puts newHash
-=======
-  puts a
->>>>>>> ea9487d23c7c63b73665c7eb47bbbcdd798bc7fa
 
 #end
